f5bec1e4c324dd26a67faa23d694fb9f6e612821
Dereje-boy <Derejeg35@gmail.com>
2024-01-25 14:59:30 +0300

    first commit of the project

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About Page</h1>
+            <p>This is an about page</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur atque praesentium ad mollitia ut modi iste quis veritatis quos assumenda, ea cupiditate esse numquam dignissimos nisi repudiandae placeat quia?</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur atque praesentium ad mollitia ut modi iste quis veritatis quos assumenda, ea cupiditate esse numquam dignissimos nisi repudiandae placeat quia?</p>
+    </>
+  );
+}
